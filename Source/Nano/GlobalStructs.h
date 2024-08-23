0447606e13d97a7f01fa94387cc5fbf46cb915f2
Chii chan <ai.chan@sipher.xyz>
2024-08-23 17:11:51 +0700

    init

---
+++ Source/Nano/GlobalStructs.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+
+#include "Actor/GameplayActor.h"
+#include "GlobalStructs.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSpawnType : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftClassPtr<AGameplayActor> ActorClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FString> Scripts;
+};
